02dda2cba082a1fad635b934fbae2e90a4b95cbe
Ilya Morozov <kvadrohan@mail.ru>
2021-01-10 14:38:53 +0300

    initialize with correct gitignore

---
+++ src/main/java/by/training/testing/controller/command/impl/GoToSignUpPageCommand.java
@@ -0,0 +1,20 @@
+package by.training.testing.controller.command.impl;
+
+import by.training.testing.controller.command.Command;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class GoToSignUpPageCommand implements Command {
+
+    private static final String SIGNUP_PAGE_URI = "WEB-INF/jsp/signUp.jsp";
+
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher(SIGNUP_PAGE_URI);
+        dispatcher.forward(req, resp);
+    }
+}
---
+++ src/main/java/by/training/testing/dao/factory/DAOFactory.java
@@ -0,0 +1,39 @@
+package by.training.testing.dao.factory;
+
+import by.training.testing.dao.*;
+import by.training.testing.dao.impl.*;
+
+public final class DAOFactory {
+    private static final DAOFactory instance = new DAOFactory();
+
+    private static final UserDAO sqlUserImpl = new UserDAOImpl();
+    private static final SubjectDAO sqlSubjectImpl = new SubjectDAOImpl();
+    private static final TestDAO sqlTestImpl = new TestDAOImpl();
+    private static final AnswerDAO sqlAnswerImpl = new AnswerDAOImpl();
+    private static final QuestionDAO sqlQuestionImpl = new QuestionDAOImpl();
+
+    private DAOFactory() {}
+    public static DAOFactory getInstance() {
+        return instance;
+    }
+
+    public UserDAO getUserDao() {
+        return sqlUserImpl;
+    }
+
+    public SubjectDAO getSubjectDao() {
+        return sqlSubjectImpl;
+    }
+
+    public TestDAO getTestDao() {
+        return sqlTestImpl;
+    }
+
+    public AnswerDAO getAnswerDao() {
+        return sqlAnswerImpl;
+    }
+
+    public QuestionDAO getQuestionDao() {
+        return sqlQuestionImpl;
+    }
+}
