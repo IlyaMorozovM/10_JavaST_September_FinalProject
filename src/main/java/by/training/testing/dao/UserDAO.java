d43ddb82e8e12b05f1256b153f8bc83272cab342
Ilya Morozov <kvadrohan@mail.ru>
2021-01-10 14:37:29 +0300

    Revert "initialize repository"
    This reverts commit 2c34791ac61637a7b2954bafc088427823e2616d.

--- src/main/java/by/training/testing/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package by.training.testing.dao;
-
-import by.training.testing.bean.User;
-import by.training.testing.dao.exception.DAOException;
-
-public interface UserDAO {
-    User signIn(String login, byte[] password) throws DAOException;
-    void signUp(String login, byte[] password, String name, String lastname, String email, int roleId) throws DAOException;
-}
--- src/main/java/by/training/testing/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package by.training.testing.service;
-
-import by.training.testing.service.exception.ServiceException;
-import by.training.testing.bean.User;
-
-public interface UserService {
-        User signIn(String login, byte[] password) throws ServiceException;
-        boolean signUp(String login, byte[] password, String name, String lastname, String email, int roleId) throws ServiceException;
-}
