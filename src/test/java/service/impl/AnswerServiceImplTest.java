e5fbb110d589ec85abf5f6036f5f1d490c6b4547
Ilya Morozov <kvadrohan@mail.ru>
2021-01-23 15:19:24 +0300

    Add wrapping to tests

--- src/test/java/service/impl/AnswerServiceImplTest.java
+++ src/test/java/service/impl/AnswerServiceImplTest.java
@@ -1,4 +1,24 @@
 package service.impl;
 
+import by.training.testing.service.exception.ServiceException;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class AnswerServiceImplTest {
+
+    @DataProvider(name = "get_parser_good")
+    public Object[][] createPositiveDataBuild() throws ServiceException {
+        return null;
+    }
+
+    @DataProvider(name = "get_parser_bad")
+    public Object[][] createNegativeDataBuild(){
+        return null;
+    }
+
+    @Test(description = "Positive scenario of parsing file",
+            dataProvider = "get_parser_good")
+    public void createCandieParserTest(String type, Class expected) throws ServiceException {
+
+    }
 }
