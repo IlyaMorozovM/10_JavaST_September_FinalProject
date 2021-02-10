dd22e069b839e1ebeb464498955d59fcb55548c6
Ilya Morozov <kvadrohan@mail.ru>
2021-02-10 22:40:19 +0300

    Add tests and comments for UserValidator class. Add 1 change in *password* international phrase.

--- src/main/java/by/training/testing/service/validator/UserValidator.java
+++ src/main/java/by/training/testing/service/validator/UserValidator.java
@@ -3,32 +3,75 @@ package by.training.testing.service.validator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * This class contains methods, that validates user parameters.
+ *
+ * @author Ilya Morozov
+ * @version	1.0
+ * @since	2020-12-20
+ */
 public class UserValidator {
-
+    
     private static final String EMAIL_PATTERN = "(\\w{3,})@(\\w+\\.)([a-z]{2,6})";
 
+    /**+
+     * This method validates user email.
+     *
+     * @param email User email.
+     * @return Result of email validation (true - email valid, otherwise - false).
+     */
     public boolean isValidEmail(String email){
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
 
+    /**+
+     * This method validates user password.
+     *
+     * @param password User password.
+     * @return Result of password validation (true - password valid, otherwise - false).
+     */
     public boolean isValidPassword(String password){
-        return password.length() >= 6 && password.length() <= 50;
+        return password.length() >= 6 && password.length() < 250;
     }
 
+    /**+
+     * This method validates user login.
+     *
+     * @param login User login.
+     * @return Result of login validation (true - login valid, otherwise - false).
+     */
     public boolean isValidLogin(String login){
         return login.length() >= 6 && login.length() <= 50;
     }
 
+    /**+
+     * This method validates user name.
+     *
+     * @param name User name.
+     * @return Result of name validation (true - name valid, otherwise - false).
+     */
     public boolean isValidName(String name){
         return name.length() >= 3 && name.length() <= 50;
     }
 
+    /**+
+     * This method validates user lastname.
+     *
+     * @param lastname User lastname.
+     * @return Result of lastname validation (true - lastname valid, otherwise - false).
+     */
     public boolean isValidLastname(String lastname){
         return lastname.length() >= 3 && lastname.length() <= 50;
     }
 
+    /**+
+     * This method validates user role.
+     *
+     * @param role User role (1 = tutor, 2 = student, 3 = admin).
+     * @return Result of role validation (true - role valid, otherwise - false).
+     */
     public boolean isValidRole(String role){
         int roleInt;
         try {
--- src/test/java/service/validator/UserValidatorTest.java
+++ src/test/java/service/validator/UserValidatorTest.java
@@ -1,4 +1,128 @@
 package service.validator;
 
+import by.training.testing.service.validator.UserValidator;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class UserValidatorTest {
+
+    UserValidator userValidator = new UserValidator();
+
+    @BeforeClass
+    public void init(){
+    }
+
+    @DataProvider(name = "email_validation")
+    public Object[][] createPositiveDataEmail() {
+        return new Object[][]{
+                {"example@gmail.com", true},
+                {"user@mail.ru", true},
+                {"example@gmail..com", false},
+                {"example@@gmail.com", false},
+                {"example@gmail_com", false}
+        };
+    }
+
+    @DataProvider(name = "password_validation")
+    public Object[][] createPositiveDataPassword() {
+        return new Object[][]{
+                {"123456", true},
+                {"qwerty", true},
+                {"hardPassword123", true},
+                {"short", false},
+                {"123", false}
+        };
+    }
+
+    @DataProvider(name = "login_validation")
+    public Object[][] createPositiveDataLogin() {
+        return new Object[][]{
+                {"Lin Ngo", true},
+                {"User Login", true},
+                {"John Smith", true},
+                {"short", false},
+                {"123", false}
+        };
+    }
+
+    @DataProvider(name = "name_validation")
+    public Object[][] createPositiveDataName() {
+        return new Object[][]{
+                {"Lin", true},
+                {"Name", true},
+                {"John", true},
+                {"12", false},
+                {"aa", false}
+        };
+    }
+
+    @DataProvider(name = "lastname_validation")
+    public Object[][] createPositiveDataLastname() {
+        return new Object[][]{
+                {"Ngo", true},
+                {"Lastname", true},
+                {"Smith", true},
+                {"aa", false},
+                {"12", false}
+        };
+    }
+
+    @DataProvider(name = "role_validation")
+    public Object[][] createPositiveDataRole() {
+        return new Object[][]{
+                {"1", true},
+                {"2", true},
+                {"3", true},
+                {"0", false},
+                {"-1", false},
+                {"4", false}
+        };
+    }
+
+    @Test(description = "Positive scenario of email validation",
+            dataProvider = "email_validation")
+    public void isValidEmailTest(String email, boolean expected) {
+        boolean actual = userValidator.isValidEmail(email);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(description = "Positive scenario of password validation",
+            dataProvider = "password_validation")
+    public void isValidPasswordTest(String password, boolean expected) {
+        boolean actual = userValidator.isValidPassword(password);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(description = "Positive scenario of login validation",
+            dataProvider = "login_validation")
+    public void isValidLoginTest(String login, boolean expected) {
+        boolean actual = userValidator.isValidLogin(login);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(description = "Positive scenario of name validation",
+            dataProvider = "name_validation")
+    public void isValidNameTest(String name, boolean expected) {
+        boolean actual = userValidator.isValidName(name);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(description = "Positive scenario of lastname validation",
+            dataProvider = "lastname_validation")
+    public void isValidLastnameTest(String lastname, boolean expected) {
+        boolean actual = userValidator.isValidLastname(lastname);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(description = "Positive scenario of role validation",
+            dataProvider = "role_validation")
+    public void isValidRoleTest(String role, boolean expected) {
+        boolean actual = userValidator.isValidRole(role);
+        Assert.assertEquals(actual, expected);
+    }
+
+
+
 }
